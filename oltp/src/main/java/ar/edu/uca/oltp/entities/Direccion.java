39667d1c834e8815c7773db1b435bb88329de711
rdipasquale <ricardo.dipasquale@gmail.com>
2021-04-20 19:35:07 -0300

    secuencia

--- oltp/src/main/java/ar/edu/uca/oltp/entities/Direccion.java
+++ oltp/src/main/java/ar/edu/uca/oltp/entities/Direccion.java
@@ -1,9 +1,16 @@
 package ar.edu.uca.oltp.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Direccion {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="DIRECCION_SEQ")
+	private int id;
 	private String calle;
 	private Integer altura;
 	private Integer codigoPostal;
@@ -44,52 +51,5 @@ public class Direccion {
 	}
 	public void setDepto(String depto) {
 		this.depto = depto;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((altura == null) ? 0 : altura.hashCode());
-		result = prime * result + ((calle == null) ? 0 : calle.hashCode());
-		result = prime * result + ((codigoPostal == null) ? 0 : codigoPostal.hashCode());
-		result = prime * result + ((depto == null) ? 0 : depto.hashCode());
-		result = prime * result + ((piso == null) ? 0 : piso.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Direccion other = (Direccion) obj;
-		if (altura == null) {
-			if (other.altura != null)
-				return false;
-		} else if (!altura.equals(other.altura))
-			return false;
-		if (calle == null) {
-			if (other.calle != null)
-				return false;
-		} else if (!calle.equals(other.calle))
-			return false;
-		if (codigoPostal == null) {
-			if (other.codigoPostal != null)
-				return false;
-		} else if (!codigoPostal.equals(other.codigoPostal))
-			return false;
-		if (depto == null) {
-			if (other.depto != null)
-				return false;
-		} else if (!depto.equals(other.depto))
-			return false;
-		if (piso == null) {
-			if (other.piso != null)
-				return false;
-		} else if (!piso.equals(other.piso))
-			return false;
-		return true;
 	}
 }
