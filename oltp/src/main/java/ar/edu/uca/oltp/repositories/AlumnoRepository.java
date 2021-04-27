6af20bbf287141083438bd2efdd390bf39ab7eae
federicostulich <51274964+federicostulich@users.noreply.github.com>
2021-04-27 17:23:01 -0300

    Create AlumnoRepository.java

---
+++ oltp/src/main/java/ar/edu/uca/oltp/repositories/AlumnoRepository.java
@@ -0,0 +1,12 @@
+package ar.edu.uca.oltp.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.uca.oltp.entities.Alumno;
+
+@Transactional
+public interface AlumnoRepository extends CrudRepository<Alumno, Integer>{
+
+}
