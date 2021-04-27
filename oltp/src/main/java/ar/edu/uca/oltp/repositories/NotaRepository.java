52d7b98be26a68fe99a329bee29580508dfaa68c
andreecueva97 <60671503+andreecueva97@users.noreply.github.com>
2021-04-27 18:27:30 -0300

    creacion repositorio nota

---
+++ oltp/src/main/java/ar/edu/uca/oltp/repositories/NotaRepository.java
@@ -0,0 +1,12 @@
+package ar.edu.uca.oltp.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.uca.oltp.entities.Cuota;
+
+@Transactional
+public interface NotaRepository extends CrudRepository<Nota, Integer>{
+
+}
