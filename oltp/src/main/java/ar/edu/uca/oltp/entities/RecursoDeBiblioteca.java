eaa44d6df2f1d796fe5379bc2a1537c7d5f4edd7
exequiel <exequiel@exequiel-Inspiron-5567>
2021-04-27 16:18:26 -0300

    Agregue mapeo de jerarquias

--- oltp/src/main/java/ar/edu/uca/oltp/entities/RecursoDeBiblioteca.java
+++ oltp/src/main/java/ar/edu/uca/oltp/entities/RecursoDeBiblioteca.java
@@ -5,10 +5,13 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
+@Inheritance(strategy =InheritanceType.TABLE_PER_CLASS)
 public class RecursoDeBiblioteca {
 	@Id
 	@GeneratedValue(strategy =GenerationType.SEQUENCE , generator = "RECBIBLIO_SEQ")
