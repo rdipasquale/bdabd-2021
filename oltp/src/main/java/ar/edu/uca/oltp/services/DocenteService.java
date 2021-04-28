fba988d9a16ca4bacedf4bf45f8d91cf5a5689e7
andreecueva97 <60671503+andreecueva97@users.noreply.github.com>
2021-04-27 22:01:50 -0300

    creando servicio docente

---
+++ oltp/src/main/java/ar/edu/uca/oltp/services/DocenteService.java
@@ -0,0 +1,18 @@
+package ar.edu.uca.oltp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.uca.oltp.entities.Alumno;
+import ar.edu.uca.oltp.repositories.AlumnoRepository;
+
+
+@Component
+@Transactional
+public class DocenteService {
+
+	
+}
