9ca07945f534e6cc3fd45ad3979a3df87958051b
federico <federico@federico-ubuntu>
2021-04-27 22:46:51 -0300

    Create EdificioRepository.java

---
+++ oltp/src/main/java/ar/edu/uca/oltp/repositories/EdificioRepository.java
@@ -0,0 +1,11 @@
+package ar.edu.uca.oltp.repositories;
+
+import javax.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+import ar.edu.uca.oltp.entities.Edificio;
+
+
+@Transactional
+public interface EdificioRepository extends CrudRepository<Edificio, Integer>{
+
+}
