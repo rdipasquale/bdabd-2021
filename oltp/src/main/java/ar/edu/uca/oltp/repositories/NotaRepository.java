1d613519e2c3c88bf3ceaaf1991255b8b7231ce8
andreecueva97 <andreecueva6@gmail.com>
2021-05-01 11:53:16 -0300

    agrego metodos a los repositorios: NotaRepositorio y ProyectoRepositorio

--- oltp/src/main/java/ar/edu/uca/oltp/repositories/NotaRepository.java
+++ oltp/src/main/java/ar/edu/uca/oltp/repositories/NotaRepository.java
@@ -1,6 +1,11 @@
 package ar.edu.uca.oltp.repositories;
 
+import ar.edu.uca.oltp.entities.Alumno;
+import ar.edu.uca.oltp.entities.Docente;
 import ar.edu.uca.oltp.entities.Nota;
+
+import java.util.Date;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -10,4 +15,36 @@ import org.springframework.data.repository.CrudRepository;
 @Transactional
 public interface NotaRepository extends CrudRepository<Nota, Integer>{
 
+	List<Nota> findByAlumno(Alumno alumno);
+	
+	List<Nota> findByDocente(Docente docente);
+	
+	List<Nota> findByFecha(Date fecha);
+	
+	Nota findById(int id);
+	
+	long count();
+	
+	long countByDocenteAndFecha(Docente docente,Date fecha);
+	
+	long countByDocenteAndAlumno(Docente docente,Alumno alumno);
+	
+	void delete(Nota nota);
+	
+	void deleteById(int id);
+	
+	void deleteAll();
+	
+	void deleteAllByFecha(Date fecha);
+	
+	List<Nota> removeByAlumno(Alumno alumno);
+	
+	boolean existsById(int id);
+	
+	boolean existsByAlumnoAndCalificacion(Alumno alumno,int calificacion);
+	
+	List<Nota> findAll();
+	
+	List<Nota> findAllByOrderByIdAsc();
+	
 }
--- oltp/src/main/java/ar/edu/uca/oltp/repositories/ProyectoRepository.java
+++ oltp/src/main/java/ar/edu/uca/oltp/repositories/ProyectoRepository.java
@@ -1,6 +1,10 @@
 package ar.edu.uca.oltp.repositories;
 
+import ar.edu.uca.oltp.entities.Carrera;
+import ar.edu.uca.oltp.entities.Director;
 import ar.edu.uca.oltp.entities.Proyecto;
+
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -10,4 +14,27 @@ import org.springframework.data.repository.CrudRepository;
 @Transactional
 public interface ProyectoRepository extends CrudRepository<Proyecto, Integer>{
 
+	List<Proyecto> findByDirector(Director director);
+	List<Proyecto> findByCarrera(Carrera carrera);
+	
+	Proyecto findById(int id);
+	
+	long count();
+	
+	long countByDirector(Director director);
+	
+	void delete(Proyecto proyecto);
+	
+	void deleteById(int id);
+	
+	void deleteAll();
+	
+	List<Proyecto> removeByDirector(Director director);
+	
+	boolean existsById(int id);
+	
+	List<Proyecto> findAll();
+	
+	List<Proyecto> findAllByOrderByIdAsc();
+	
 }
