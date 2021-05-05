8f8c51002ca8c5887daa7aa47c53c8a119268720
Fs <fede.stulich@gmail.com>
2021-05-04 21:37:21 -0300

    Agregue findByNombre en edificioRepository

--- oltp/src/main/java/ar/edu/uca/oltp/repositories/EdificioRepository.java
+++ oltp/src/main/java/ar/edu/uca/oltp/repositories/EdificioRepository.java
@@ -2,10 +2,14 @@ package ar.edu.uca.oltp.repositories;
 
 import javax.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
+
 import ar.edu.uca.oltp.entities.Edificio;
 
 
 @Transactional
 public interface EdificioRepository extends CrudRepository<Edificio, Integer>{
+	
+	public Edificio findByNombre(String nombre) ;
+
 
 }
