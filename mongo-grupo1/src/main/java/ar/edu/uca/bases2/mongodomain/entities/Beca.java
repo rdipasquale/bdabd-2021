bb73265f66a137c153e15a50f9b4f020890f5339
Fs <fede.stulich@gmail.com>
2021-05-11 22:08:18 -0300

    Agregue documento Beca

---
+++ mongo-grupo1/src/main/java/ar/edu/uca/bases2/mongodomain/entities/Beca.java
@@ -0,0 +1,65 @@
+package ar.edu.uca.bases2.mongodomain.entities;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Beca {
+	
+	@Id
+	private String id;
+	private Date fechaVencimiento;
+	private Alumno alumno;
+	
+	public Beca() {
+		super();
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Beca other = (Beca) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+	public void setFechaVencimiento(Date fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+	public Alumno getAlumno() {
+		return alumno;
+	}
+	public void setAlumno(Alumno alumno) {
+		this.alumno = alumno;
+	}
+	
+	
+	
+
+}
+
