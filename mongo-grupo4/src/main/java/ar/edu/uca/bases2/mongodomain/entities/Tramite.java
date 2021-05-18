1fd703c93dbcce395e7d09be816e083d6397248d
tomasnozica <tomas.nozica@gmail.com>
2021-05-18 20:36:02 -0300

    set up Mongo & classes

---
+++ mongo-grupo4/src/main/java/ar/edu/uca/bases2/mongodomain/entities/Tramite.java
@@ -0,0 +1,15 @@
+package ar.edu.uca.bases2.mongodomain.entities;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Tramite {
+	@Id
+	private int id;
+	protected EstadoTramite estado;
+	private Date fechaInicio;
+	protected Date fechaCierre;
+}
